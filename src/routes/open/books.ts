ff5f0b238ed05e1caa0b84f9439d9a0a9793370f
BRODI MATHERLY <69179454+amatherly@users.noreply.github.com>
2024-05-17 18:47:58 -0700

    Revert "Merge branch 'main' into bugFixez"
    This reverts commit 5ba3284994424064f2dc68b6eebdb23bb774b6d6, reversing
    changes made to 1dafdc3e1763bac8513668b7e30b1705d297b2ba.

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -1,5 +1,4 @@
 
-
 import {pool} from "../../core/utilities";
 import express, {NextFunction, Request, Response, Router} from 'express';
 const format = (resultRow) =>
@@ -9,7 +8,6 @@ const bookFormat = (resultRow) => `${resultRow.isbn13}, ${resultRow.authors}, ${
 
 const isbnFormat = (resultRow) =>
     `{${resultRow.isbn13}}] says: ${resultRow.message}`;
-
 
 const booksRouter: Router = express.Router();
 
@@ -64,116 +62,6 @@ function formatBooks(books): IBook[] {
 
 
 /**
- * @api {get} /books/get_all_books Request to retrieve all books
- *
- * @apiDescription Request to retrieve all books in the DB
- *
- * @apiName GetAllBooks
- * @apiGroup Books
- *
- *
- * @apiSuccess {String[]} books the aggregate of all books as the following string:
- *      "{<code>isbn13</code>: <code>books isbn13</code>,
- *      <code>authors</code>: <code>books authors</code>,
- *      <code>publication</code>: <code>books publication year</code>,
- *      <code>original_title</code>: <code>books original title</code>,
- *      <code>title</code>: <code>books title</code>,
- *      <code>ratings</code>: {
- *          <code>average</code>: <code>books average rating</code>,
- *          <code>count</code>: <code>books count of total ratings</code>,
- *          <code>rating_1</code>: <code>books count of 1 star ratings</code>,
- *          <code>rating_2</code>: <code>books count of 2 star ratings</code>,
- *          <code>rating_3</code>: <code>books count of 3 star ratings</code>,
- *          <code>rating_4</code>: <code>books count of 4 star ratings</code>,
- *          <code>rating_5</code>: <code>books count of 5 star ratings</code>
- *      },
- *      <code>icons</code>: {
- *          <code>large</code>: <code>books large icon</code>,
- *          <code>small</code>: <code>books small icon</code>,
- *      }}"
- *
- *
- * @apiError (404: No Books Found) {string} message "No Books found"
- */
-booksRouter.get('/get_all_books', (request: Request, response: Response) => {
-    // TODO pagination
-    const theQuery = 'SELECT * FROM books';
-    const values = [];
-
-    pool.query(theQuery, values)
-        .then((result) => {
-            if (result.rowCount > 0) {
-                response.send({
-                    books: formatBooks(result.rows),
-                });
-            } else {
-                response.status(404).send({
-                    message: `No Books found`,
-                    code: 404,
-                });
-            }
-        })
-        .catch((error) => {
-            //log the error
-            console.error('DB Query error on GET by priority');
-            console.error(error);
-            response.status(500).send({
-                message: 'server error - contact support',
-            });
-        });
-});
-
-/**
- * @api {put} /books/update_by_ratings Request to update a books ratings
- *
- * @apiDescription Request to add a specific star rating of a book in the DB
- *
- * @apiName PutRatingsBook
- * @apiGroup Books
- *
- * @apiBody {integer} rating the star rating
- * @apiBody {bigint} isbn13 the isbn number of the book to add the rating to
- *
- * @apiSuccess {String} books "Rating successfully added"
- *
- * @apiError (404: ISBN Not Found) {String} message "ISBN not found, rating not added"
- */
-booksRouter.put(
-    '/update_by_ratings',
-    (request: Request, response: Response, next: NextFunction) => {
-        const rating = request.body.rating;
-        const theQuery = `UPDATE books SET rating_${rating}_star = rating_${rating}_star + 1, rating_count = rating_count + 1 WHERE isbn13 = $1`;
-        const values = [request.body.isbn13];
-
-        pool.query(theQuery, values)
-            .then((result) => {
-                if (result.rowCount == 1) {
-                    response.send({
-                        message: 'Rating successfully added',
-                        code: 200,
-                    });
-                } else {
-                    response.status(404).send({
-                        message: 'ISBN not found, rating not added',
-                        code: 404,
-                    });
-                }
-            })
-            .catch((error) => {
-                //log the error
-                console.error('DB Query error on PUT');
-                console.error(error);
-                response.status(500).send({
-                    message: 'server error - contact support',
-                });
-            });
-    }
-);
-
-
-
-
-/**
  * @api {get} /books/get_by_rating/:rating Retrieve books by average rating
  * 
  * @apiDescription Retrieves books from the database that have an average rating of the rating specified, rounded down to the nearest integer.
@@ -190,10 +78,12 @@ booksRouter.put(
  * @apiError (404: No Books Found) {String} message "No books found with that rating."
  * @apiError (500: Server Error) {String} message "Server error - contact support."
  */
+booksRouter.get('/get_by_rating/:rating', (request, response) => {
+    if (!request.query.rating) {
+        return response.status(400).send({ message: "Rating parameter is required." });
+    }
 
-booksRouter.get('/get_by_rating', (request: Request, response: Response) => {
-    const ratingParam = request.query.rating;
-    const rating = parseInt(ratingParam as string)
+    const rating = parseInt(request.query.rating as string);
     if (isNaN(rating) || rating < 1 || rating > 5) {
         return response.status(400).send({ message: "Invalid rating parameter. Please specify a rating between 1 and 5." });
     }
@@ -204,15 +94,14 @@ booksRouter.get('/get_by_rating', (request: Request, response: Response) => {
     pool.query(theQuery, values)
         .then((result) => {
             if (result.rows.length === 0) {
-                return response.status(200).send({ message: "No books found with that rating." });
+                return response.status(404).send({ message: "No books found with that rating." });
             }
             response.send({
                 books: formatBooks(result.rows)
             });
         })
         .catch((error) => {
-
-            console.error('DB Query error on GET /get_by_rating');
+            console.error('DB Query error on GET /book_by_rating');
             console.error(error);
             response.status(500).send({
                 message: 'Server error - contact support',
@@ -242,13 +131,12 @@ booksRouter.get('/get_by_rating', (request: Request, response: Response) => {
  * @apiError (404: No Books Found) {String} message "No books found within that date range."
  * @apiError (500: Server Error) {String} message "Server error - contact support."
  */
-booksRouter.delete('/delete_by_range', (request, response) => {
+booksRouter.delete('/books/delete_by_range/:min/:max', (request, response) => {
     const theQuery = 'DELETE FROM books WHERE publication_year >= $1 AND publication_year <= $2';
     const values = [request.query.min, request.query.max];
 
-    const min = parseInt(request.query.min as string, 10);
-    const max = parseInt(request.query.max as string, 10);
-
+    const min = parseInt(request.query.min as string);
+    const max = parseInt(request.query.max as string);
 
     if (isNaN(min) || isNaN(max) || min > max) {
         return response.status(400).send({ message: "Invalid date parameters. Please specify a min and max publication year. min must be less than max." });
@@ -483,3 +371,5 @@ booksRouter.get('/get_by_isbn', (request: Request, response: Response) => {
 });
 
 export {booksRouter};
+
+
